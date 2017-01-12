f4f94e6797882308684b5056287bfcb762383fba
taquaki-satwo <satoreco@gmail.com>
2017-01-12 23:30:03 +0900

    Add plugins

--- sample.js
+++ sample.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 const postcss = require('postcss');
 const atImport = require('postcss-import');
+const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const stylelint = require('stylelint');
+const reporter = require('postcss-reporter');
 
 fs.readFile('import.css', (err, css) => {
-  postcss([atImport, cssnano])
-    .process(css)
+  postcss([
+    atImport,
+    stylelint,
+    reporter,
+    autoprefixer,
+    cssnano
+  ]).process(css)
     .then(result => {
       fs.writeFile('app.css', result.css);
     });
